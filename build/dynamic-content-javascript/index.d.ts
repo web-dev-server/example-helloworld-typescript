e653496eaa44d13decdc6f17d2b39d93dc09dd55
Tom Flídr <tomflidr@gmail.com>
2020-04-08 09:15:21 +0200

    declaration file update

--- build/dynamic-content-javascript/index.d.ts
+++ build/dynamic-content-javascript/index.d.ts
@@ -2,8 +2,8 @@ export = App;
 declare class App {
     server: any;
     counter: number;
-    Start(server: any, firstRequest: any, firstResponse: any): Promise<void>;
-    Stop(server: any): Promise<void>;
-    HttpHandle(request: any, response: any): Promise<void>;
+    public Start(server: any, firstRequest: any, firstResponse: any): Promise<void>;
+    public Stop(server: any): Promise<void>;
+    public HttpHandle(request: any, response: any): Promise<void>;
     loadHtmlFile(fullPath: any, resolve: any, reject: any): void;
 }
